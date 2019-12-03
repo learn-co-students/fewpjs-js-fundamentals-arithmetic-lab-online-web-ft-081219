d021c31afd7575e78298c28872e6f53849565c65
KHong <albtrossfacefall@gmail.com>
2019-12-03 03:38:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 9%5;
+
+let max = Math.max(4, 8, 12, 16, 20);
