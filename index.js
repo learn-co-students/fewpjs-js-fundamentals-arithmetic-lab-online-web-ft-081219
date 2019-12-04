36658bf9eb76299c70471edf747c96fb13111d0c
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-03 21:32:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10) + 1;
+let mod = 10 % 6;
+let max = Math.max(1, 2, 20);
